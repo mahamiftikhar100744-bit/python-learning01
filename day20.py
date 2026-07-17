4c80cda122081a9db66d5adc1edeaa3fc2c8d565
mahamiftikhar100744-bit <mahamiftikhar100744@gmail.com>
2026-07-18 00:10:59 +0500

    Create day20.py

---
+++ day20.py
@@ -0,0 +1,15 @@
+def gmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def greater(a, b):
+    if(a>b):
+        print("a is greater number")
+    else:
+        print("a is greater number")
+
+a= int(input("enter an integer number: "))
+b= int(input("enter an integer number: "))
+
+gmean(a, b)
+
+greater(a, b)

c7e2ee77c8d49df23fbf0d5a589f58a94fa1b1ac
mahamiftikhar100744-bit <mahamiftikhar100744@gmail.com>
2026-07-17 23:15:35 +0500

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,13 @@
+x= int(input("Enter a number: "))
+for i in range(x):
+    print(i)
+y= int(input("Enter a number: "))
+for a in range(y):
+   print(a+1)
+z= int(input("Enter a number: "))
+for b in range(1,z+1):
+   print(b)
+j= int(input("Enter a number: "))
+for c in range(1,j,2):
+    print(c)
+    

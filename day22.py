c23fe1e098853fd957d56fb84203922fec175e3f
mahamiftikhar100744-bit <mahamiftikhar100744@gmail.com>
2026-07-18 11:39:48 +0500

    Create day22.py

---
+++ day22.py
@@ -0,0 +1,6 @@
+animels = ["cat", "dog", "bat", "pig", "horse", "donkey", "goat"]
+
+print(animels[3:7])
+print(animels[-7:-2])
+print(animels[len(animels)-7:len(animels)-2])
+print(animels[7-7:7-2])

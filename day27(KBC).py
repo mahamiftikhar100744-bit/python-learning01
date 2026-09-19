54e6cad836ba7d9e4e32ba9b683bcae1a662bdff
mahamiftikhar100744-bit <mahamiftikhar100744@gmail.com>
2026-09-19 10:42:25 +0500

    Create day27(KBC).py

---
+++ day27(KBC).py
@@ -0,0 +1,121 @@
+print("WELLCONE, To our show.\n\t\t      \"Kaun Banega Crorepati\"")
+print("\n\nHere you can win \"MONEY\" by just answering some question's.")
+print("Also there will be some rules.")
+print("1. Only one player can play at a time.")
+print("2. You have only 3 life line, Google it, Ask someone but only one person, Hint.")
+print("3. Each life line will be used only once.")
+print("4. You have only three life's. After 3 incorrect answer's and the game will be over for you.")
+print("5. After answering each question. You will be proceeded to the next question.")
+print("If it's clear, Then shall we begin.\n")
+
+print("Here's your 1st question. For Rs. 4.\n")
+print("Q1. Which planet is known as the Red Planet?")
+print("A) Venus\tB) Mars\nC) Jupiter\tD) Saturn")
+ans1=input()
+if "mars" in ans1:
+    print("Correct. You won Rs. 4")
+else:
+    print("Incorrect.")
+
+print("Here's your 2nd question. For Rs. 16.\n")
+print("Q2. Which is the largest mammal?")
+print("A) Elephant\tB) Blue Whale\nC) Giraffe\tD) Hippopotamus")
+ans2=input()
+if "blue whale" in ans2:
+    print("Correct. You won Rs. 16")
+else:
+    print("Incorrect.")
+
+print("Here's your 3nd question. For Rs. 64.\n")
+print("Q3. Which scientist developed the theory of relativity?")
+print("A) Isaac Newton\tB) Albert Einstein\nC) Niels Bohr\tD) Galileo Galilei")
+ans3=input()
+if "albert einstein" in ans3:
+    print("Correct. You won Rs. 64")
+else:
+    print("Incorrect.")
+
+print("Here's your 4th question. For Rs. 256.\n")
+print("Q4. Which Mughal emperor built the Red Fort?")
+print("A) Akbar\tB) Aurangzeb\nC) Shah Jahan\tD) Humayun")
+ans4=input()
+if "shah jahan" in ans4:
+    print("Correct. You won Rs. 256")
+else:
+    print("Incorrect.")
+
+print("Here's your 5th question. For Rs. 1,024.\n")
+print("Q5. Which is the largest gland in the human body?")
+print("A) Pancreas\tB) Liver\nC) Thyroid\tD) Spleen")
+ans5=input()
+if "liver" in ans5:
+    print("Correct. You won Rs. 1,024")
+else:
+    print("Incorrect.")
+
+print("Here's your 6th question. For Rs. 4,096.\n")
+print("Q6. Which sea is the saltiest large body of water on Earth?")
+print("A) Red Sea\tB) Mediterranean SeaLiver\nC) Baltic Sea\tD) Black Sea")
+ans6=input()
+if "red sea" in ans6:
+    print("Correct. You won Rs. 4,096")
+else:
+    print("Incorrect.")
+
+print("We are half way done.")
+
+print("Here's your 7th question. For Rs. 65,536.\n")
+print("Q7. Which is the world's deepest ocean trench?")
+print("A) Java Trench\tB) Puerto Rico Trench\nC) Mariana Trench\tD) Tonga Trench")
+ans7=input()
+if "mariana trench" in ans7:
+    print("Correct. You won Rs. 65,536")
+else:
+    print("Incorrect.")
+
+print("Here's your 8th question. For Rs. 262,144.\n")
+print("Q8. Which country is the largest producer of coffee in the world?")
+print("A) Vietnam\tB) Colombia\nC) Brazil\tD) Ethiopia")
+ans8=input()
+if "brazil" in ans8:
+    print("Correct. You won Rs. 262,144")
+else:
+    print("Incorrect.")
+
+print("Here's your 9th question. For Rs. 1,048,576.\n")
+print("Q9. Which chemical element has the atomic number 79?")
+print("A) Platinum\tB) Mercury\nC) Gold\tD) Silver")
+ans9=input()
+if "gold" in ans9:
+    print("Correct. You won Rs. 1,048,576")
+else:
+    print("Incorrect.")
+
+print("Here's your 10th question. For Rs. 262,144.\n")
+print("Q10. Which planet has the largest number of known moons?")
+print("A) Jupiter\tB) Saturn\nC) Uranus\tD) Neptune")
+ans10=input()
+if "saturn" in ans10:
+    print("Correct. You won Rs. 262,144")
+else:
+    print("Incorrect.")
+
+print("Here's your 11th question. For Rs. 4,194,30.\n")
+print("Q11. Which country is known as the \"Land of a Thousand Lakes\"?")
+print("A) Sweden\tB) Norway\nC) Finland\tD) Canada")
+ans11=input()
+if "finland" in ans11:
+    print("Correct. You won Rs. 4,194,304")
+else:
+    print("Incorrect.")
+
+print("Here's your 12th question. For Rs. 16,777,216.\n")
+print("Q12. Which scientist first accurately calculated the circumference of the Earth using shadows?")
+print("A) Ptolemy\tB) Archimedes\nC) Eratosthenes\tD) Euclid")
+ans12=input()
+if "eratosthenes" in ans12:
+    print("Correct. You won Rs. 16,777,216")
+else:
+    print("Incorrect.")
+
+    print("\nCongratulation, You become a Crorepati")
